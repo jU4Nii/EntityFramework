99b33bce808f7645532276ab8bfbe1e08ef8eed7
jU4Nii <juanibonora2004@gmail.com>
2025-09-16 16:40:58 -0300

    ConexionBase

--- EjercicioPruebaBD/Consola/obj/Debug/net8.0/Consola.AssemblyInfo.cs
+++ EjercicioPruebaBD/Consola/obj/Debug/net8.0/Consola.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c166c0f159fac49f7fa948b74df181f43358509")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
