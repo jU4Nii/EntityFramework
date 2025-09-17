db561526322eca75a03744dbdbce48a6973a91bc
jU4Nii <juanibonora2004@gmail.com>
2025-09-17 19:00:23 -0300

    FinalizacionPracticaVehiculosViajes

--- EjercicioPruebaBD/Consola/Program.cs
+++ EjercicioPruebaBD/Consola/Program.cs
@@ -8,6 +8,7 @@ namespace Consola
         static void Main(string[] args)
         {
             Vehiculo vehiculo = new Vehiculo("BMW", 2015, "AGV200", 4, "V12");
+            Vehiculo vehiculo1 = new Vehiculo("Audi", 2016, "EOW240", 4, "V12");
         }
     }
 }
--- EjercicioPruebaBD/Consola/obj/Debug/net8.0/Consola.AssemblyInfo.cs
+++ EjercicioPruebaBD/Consola/obj/Debug/net8.0/Consola.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c166c0f159fac49f7fa948b74df181f43358509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b33bce808f7645532276ab8bfbe1e08ef8eed7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
